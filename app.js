31be921db16dbf0a8490b3ee86bbd85fdbbacc6f
Benjamin Smith <ben.smith@10klt.com>
2016-12-07 16:57:54 -0800

    add __dirname

--- app.js
+++ app.js
@@ -22,12 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( { extended: true } ));
 
 
-app.use(express.static('./client/build'));
-/*
-app.get('/*', function (req, res, next) {
-    res.sendFile(path.join(__dirname, './client/build', 'index.html'));
-});
-*/
+app.use(express.static(path.join(__dirname, './client/build')));
 
 //enable routes
 app.use(employeeRoute);
