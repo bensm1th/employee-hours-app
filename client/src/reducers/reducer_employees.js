e34848ea4693e500808b9cc318d01032e7bb6d6c
Benjamin Smith <ben.smith@10klt.com>
2016-12-07 16:41:42 -0800

    change state

--- client/src/reducers/reducer_employees.js
+++ client/src/reducers/reducer_employees.js
@@ -3,7 +3,7 @@ import { EMPLOYEES_FETCH } from '../actions/types';
 export default function(state = [], action) {
     switch(action.type) {
         case EMPLOYEES_FETCH:
-            return [...state, ...action.payload.data.employees];
+            return [...action.payload.data.employees];
         default:
             return state;
     }
