977e91d80b73ef2f3e47d4a421e769ca5e86e9ff
Benjamin Smith <ben.smith@10klt.com>
2016-12-07 17:33:09 -0800

    move routes back

--- app.js
+++ app.js
@@ -21,20 +21,22 @@ mongoose.connect(process.env.TLC_DB);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( { extended: true } ));
 
+
+
+
+app.use(express.static(path.join(__dirname, './client/build')));
+/*
+app.all('/*', function (req, res, next) {
+    console.log('all hit');
+        
+    //res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+});
+*/
 //enable routes
 app.use(employeeRoute);
 app.use(timestampRoute);
 app.use(hoursRoute);
 app.use(authRoute);
-app.use(express.static(path.join(__dirname, './client/build')));
-
-app.all('/*', function (req, res, next) {
-    console.log('all hit')
-    res.sendFile(path.join(__dirname, './client/build', 'index.html'));
-});
-
-
-
 
 app.listen(port, function() {
     console.log('tlc listening on port ' + port);
